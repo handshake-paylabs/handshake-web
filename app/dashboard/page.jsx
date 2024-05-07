269b5c66f5f492f387127e76f87495ead4f67a13
MirandaKing <miranda20199712@gmail.com>
2024-05-08 00:47:21 +0530

    solve documnent not found error

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -7,7 +7,7 @@ import Avatar from "@devneser/gradient-avatar";
 import Header from "../components/header/Header";
 export default function Dashboard() {
   const { address } = useAccount();
-  const someElement = document.querySelector("#avatar");
+  // const someElement = document.querySelector("#avatar");
   //   useEffect(() => {
   //     createWeb3Avatar(someElement, "0x11Ed0AC7D6142481E459B6e5d4bfB5646277796f");
   //   }, [someElement]);
