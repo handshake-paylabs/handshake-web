6300b2e9febddd13d10dc4a07bc36fbc98660d20
MirandaKing <miranda20199712@gmail.com>
2024-06-28 22:17:26 +0530

    deployment error resolved

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,8 +1,18 @@
 "use client";
 
-import Header from "../components/header/Header";
-import TransactionTypes from "../components/TransactionTypes";
+// import Header from "../components/header/Header";
+// import TransactionTypes from "../components/TransactionTypes";
+import dynamic from "next/dynamic";
 
+const Header = dynamic(() => import("../components/header/Header"), {
+  ssr: false,
+});
+const TransactionTypes = dynamic(
+  () => import("../components/TransactionTypes"),
+  {
+    ssr: false,
+  }
+);
 export default function Dashboard() {
   return (
     <div className="main-dashboard">
